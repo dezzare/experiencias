96c32cd1738c509fe3358e80f22da50815fa88f6
Pedro de Mesquita Bittencourt <djojoo@gmail.com>
2022-01-29 16:43:36 -0300

    Avançando no curso de JS webpack express

---
+++ Udemy Curso JS TS udemy/Node/aula 6 routes express/server.js
@@ -0,0 +1,21 @@
+//npx nodemon server.js
+
+
+const express = require('express');
+const app = express();
+const routes = require('./routes')
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs'); //npm i ejs
+
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+});
