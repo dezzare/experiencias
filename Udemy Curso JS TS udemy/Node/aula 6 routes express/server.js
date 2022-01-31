c5d8703cea7371b057be148d59ad1bdd81671e03
Pedro de Mesquita Bittencourt <djojoo@gmail.com>
2022-01-31 11:53:36 -0300

    Estudando integração Node+Express+mongoose com MongoDB

--- Udemy Curso JS TS udemy/Node/aula 6 routes express/server.js
+++ Udemy Curso JS TS udemy/Node/aula 6 routes express/server.js
@@ -1,8 +1,17 @@
 //npx nodemon server.js
 
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+  console.log('Conectado ao BD')
+  app.emit('Pronto')
+  })
+  .catch(e => console.log(e)); //imprime erro, se ocorrer
+
 const routes = require('./routes')
 const path = require('path')
 
@@ -15,7 +24,11 @@ app.set('view engine', 'ejs'); //npm i ejs
 
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('Acessar http://localhost:3000');
-  console.log('Servidor executando na porta 3000');
+app.on('Pronto', () => {
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+  });
 });
+
+
--- Udemy Curso JS TS udemy/Node/aula 6 routes express/src/controllers/homeController.js
+++ Udemy Curso JS TS udemy/Node/aula 6 routes express/src/controllers/homeController.js
@@ -1,3 +1,12 @@
+const HomeModel = require('../models/HomeModel')
+
+HomeModel.create({
+  titulo: 'Um título de teste',
+  descricao: 'Descricao de teste'
+})
+.then(dados => console.log(dados))
+.catch(e => console.log(e))
+
 exports.paginaInicial = (req, res) => {
   res.render('index');
 };
