a97a9f9f305844506197273494cc9677b3013516
Pedro de Mesquita Bittencourt <djojoo@gmail.com>
2022-03-08 09:58:57 -0300

    lendo mais um pouco sobre Rust

---
+++ Livro - The Book (rust)/Cap-7-Managing-Projects/target/debug/deps/Cap_7_Managing_Projects-3bba206b50532fa6.d
@@ -0,0 +1,5 @@
+/home/pedro/Documentos/Dev/Projetos no Github/experiencias/Livro - The Book (rust)/Cap-7-Managing-Projects/target/debug/deps/Cap_7_Managing_Projects-3bba206b50532fa6.rmeta: src/main.rs
+
+/home/pedro/Documentos/Dev/Projetos no Github/experiencias/Livro - The Book (rust)/Cap-7-Managing-Projects/target/debug/deps/Cap_7_Managing_Projects-3bba206b50532fa6.d: src/main.rs
+
+src/main.rs:
---
+++ Livro - The Book (rust)/Cap-7-Managing-Projects/target/debug/deps/Cap_7_Managing_Projects-9a07a8a439bc7c47.d
@@ -0,0 +1,5 @@
+/home/pedro/Documentos/Dev/Projetos no Github/experiencias/Livro - The Book (rust)/Cap-7-Managing-Projects/target/debug/deps/Cap_7_Managing_Projects-9a07a8a439bc7c47.rmeta: src/main.rs
+
+/home/pedro/Documentos/Dev/Projetos no Github/experiencias/Livro - The Book (rust)/Cap-7-Managing-Projects/target/debug/deps/Cap_7_Managing_Projects-9a07a8a439bc7c47.d: src/main.rs
+
+src/main.rs:
