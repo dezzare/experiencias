d1d1f37ab496fb4f3ab5e21b89758dc96151ffc9
Pedro de Mesquita Bittencourt <42898870+dezzare@users.noreply.github.com>
2022-03-15 21:49:10 -0300

    Delete Cursos - Udemy/Curso Rust/Section_2_The_Basics/target directory

--- Cursos - Udemy/Curso Rust/Section_2_The_Basics/target/debug/deps/Section_2_The_Basics-d2b173c83c7e9690.d
+++
@@ -1,5 +0,0 @@
-/home/pedro/Documentos/Dev/Projetos no Github/experiencias/Cursos - Udemy/Curso Rust/Section_2_The_Basics/target/debug/deps/Section_2_The_Basics-d2b173c83c7e9690: src/main.rs
-
-/home/pedro/Documentos/Dev/Projetos no Github/experiencias/Cursos - Udemy/Curso Rust/Section_2_The_Basics/target/debug/deps/Section_2_The_Basics-d2b173c83c7e9690.d: src/main.rs
-
-src/main.rs:
