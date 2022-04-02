ea21436a6c0d323e903548d97237adca208c79d1
Pedro de Mesquita Bittencourt <djojoo@gmail.com>
2022-04-02 05:21:34 -0300

    terminado seção 16 do curso de rust

---
+++ Cursos - Udemy/Curso Rust/section_16_unsafe_code/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // unsafe code = nome já diz tudo
+    let mut num = 5;
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    // não é necessário colocar tudo como unsafe
+    unsafe {
+        println!("r1 is {:?}", *r1);
+        println!("r2 is {:?}", *r2);
+    }
+}
